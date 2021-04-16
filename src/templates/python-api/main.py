6179795f32fd7bd926b8cbfc791f18f8e12bf690
Anthony Chu <anthony@anthonychu.ca>
2021-04-16 13:58:47 -0700

    feat: add fastapi docs

--- src/templates/python-api/main.py
+++ src/templates/python-api/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from swa_api import database
-app = FastAPI()
+app = FastAPI(docs_url="/api/.docs", openapi_url="/api/openapi.json")
 
 
 @app.get("/api/hello")
 async def root():
     return {"message": "Hello World"}
-    
