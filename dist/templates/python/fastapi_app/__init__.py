dfe2b506491e98ea5d80d872aae068cccc3e26f3
Anthony Chu <anthony@anthonychu.ca>
2021-04-20 17:05:09 -0700

    feat: commit latest pyhack changes

--- dist/templates/python/fastapi_app/__init__.py
+++ dist/templates/python/fastapi_app/__init__.py
@@ -1,7 +1,19 @@
 import logging
 
 import azure.functions as func
-from ..main import app
+import os.path
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+if (os.path.isfile(os.path.join(dir_path, "..", "..", "api", "main.py"))):
+    print("Debugging original source")
+    import sys
+    sys.path.append(os.path.join(dir_path, "..", ".."))
+    from api.main import app
+else:
+    print("Debugging copied source")
+    from ..main import app
+
 from ._http_asgi import AsgiMiddleware
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
